1f576436d8b1ef682ea9af84fcf62dbd6a93c1f0
ap <951984189@qq.com>
2020-05-21 00:43:25 +0800

    优化菜单

--- ssh/src/components/xterm.vue
+++ ssh/src/components/xterm.vue
@@ -3,13 +3,7 @@
     <!-- <button @click="test">测试13123</button> -->
     <img @click="showMenu" src="@/assets/menu.png" />
     <div v-if="showTerm" id="terminal"></div>
-    <el-drawer
-      v-if="drawer"
-      title="我是标题"
-      direction="rtl"
-      :visible.sync="drawer"
-      :with-header="false"
-    >
+    <el-drawer title="我是标题" direction="rtl" :visible.sync="drawer" :with-header="false">
       <el-menu
         :default-active="activeIndex"
         class="el-menu-demo"
@@ -18,8 +12,34 @@
         text-color="#fff"
         active-text-color="#ffd04b"
       >
-        <el-menu-item index="1">连接</el-menu-item>
-        <el-menu-item index="2">命令</el-menu-item>
+        <el-menu-item index="1">
+          <i class="el-icon-s-help"></i>
+          <span slot="title">连接</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-s-flag"></i>
+          <span slot="title">命令</span>
+        </el-menu-item>
+      </el-menu>
+      <el-menu
+        :default-active="activeIndex"
+        mode="vertical"
+        background-color="#253238"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="height: 460px;overflow-y: scroll;"
+      >
+        <el-menu-item
+          v-for="n in 20"
+          :key="n"
+          :index="n.toString()"
+          style=" width: 100%;text-align: left;"
+        >
+          <span slot="title">阿里云服务器{{n}}</span>
+        </el-menu-item>
+        <!-- <el-menu-item index="2" style=" width: 100%;text-align: left;">
+          <span slot="title">华为云服务器</span>
+        </el-menu-item>-->
       </el-menu>
     </el-drawer>
   </div>
@@ -67,7 +87,7 @@ export default {
       const fitAddon = new FitAddon();
       this.term.loadAddon(fitAddon);
       this.term.open(document.getElementById("terminal"));
-      this.term.write("COOL-AI SSH \r\n");
+      this.term.write("欢迎使用 COOL-AI SSH \r\n");
       this.term.focus();
       fitAddon.fit();
 
@@ -82,13 +102,21 @@ export default {
             stream.on("data", data => {
               this.term.write(data.toString("utf-8"));
             });
+            stream.on("close", function(code, signal) {
+              console.log(
+                "Stream :: close :: code: " + code + ", signal: " + signal
+              );
+              this.term.write("长时间未操作，已失去连接，请重连！ \r\n");
+              this.sshClient.end();
+            });
           });
         })
         .connect({
           host: "123.57.135.188",
           port: 22,
           username: "root",
-          password: "abc123456+"
+          password: "abc123456+",
+          keepaliveInterval: 10000
         });
     }
   },
@@ -105,15 +133,22 @@ export default {
 /deep/ .el-drawer__body {
   background-color: #253238;
 }
-.el-menu.el-menu--horizontal{
+.el-menu.el-menu--horizontal {
   border-bottom: none;
+}
+.el-menu-demo {
+  width: 100%;
+}
+.el-menu-item {
+  width: 50%;
+  text-align: center;
 }
 .terminal {
   width: 100%;
   img {
     position: absolute;
     width: 25px;
-    z-index: 1000000;
+    z-index: 10;
     right: 0;
     bottom: 0;
     margin: 0 10px 10px 0px;
