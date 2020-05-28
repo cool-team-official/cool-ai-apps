072de08a1a11d67885616c7ad3126db42a83232e
啊平 <951984189@qq.com>
2020-05-28 20:07:32 +0800

    1111

--- ssh/vue.config.js
+++ ssh/vue.config.js
@@ -1,16 +1,37 @@
-
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackHookPlugin = require('webpack-hook-plugin')
 module.exports = {
     publicPath: './',
+    pluginOptions: {
+        electronBuilder: {
+            externals: [
+                'ssh2'
+            ]
+        }
+    },
     configureWebpack: {
         plugins: [
-            new CopyWebpackPlugin([
-                { from: 'app.json', to: 'app.json' },
-                { from: './src/preload.js', to: 'preload.js' },
-                { from: './src/assets/logo.png', to: 'logo.png' },
-                { from: 'README.md', to: 'README.md' },
-            ])
+            new CopyWebpackPlugin([{
+                    from: 'app.json',
+                    to: 'app.json'
+                },
+                {
+                    from: './src/preload.js',
+                    to: 'preload.js'
+                },
+                {
+                    from: './src/assets/logo.png',
+                    to: 'logo.png'
+                },
+                {
+                    from: 'README.md',
+                    to: 'README.md'
+                },
+            ]),
+            new WebpackHookPlugin({
+                // onBuildStart:['echo "Webpack Start"'],
+                onBuildEnd:['cd dist']
+              })
         ]
     }
 }
-        
