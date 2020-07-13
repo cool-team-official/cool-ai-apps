6a5776cb8c46193ab2f06ca96207d43fb04717bc
ap <951984189@qq.com>
2020-07-13 22:31:49 +0800

    111

--- ssh/vue.config.js
+++ ssh/vue.config.js
@@ -1,32 +1,25 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
-  publicPath: "./",
-  configureWebpack: {
-    module: {
-      rules: [{
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: /node_modules/
-      }, ]
-    },
-    plugins: [
-      new CopyWebpackPlugin([{
-          from: "app.json",
-          to: "app.json"
-        },
-        {
-          from: "./src/preload.js",
-          to: "preload.js"
-        },
-        {
-          from: "./src/assets/logo.png",
-          to: "logo.png"
-        },
-        {
-          from: "README.md",
-          to: "README.md"
-        }
-      ])
-    ]
-  }
+    publicPath: "./",
+    configureWebpack: {
+        plugins: [
+            new CopyWebpackPlugin([{
+                    from: "app.json",
+                    to: "app.json"
+                },
+                {
+                    from: "./src/preload.js",
+                    to: "preload.js"
+                },
+                {
+                    from: "./src/assets/logo.png",
+                    to: "logo.png"
+                },
+                {
+                    from: "README.md",
+                    to: "README.md"
+                }
+            ])
+        ]
+    }
 };
